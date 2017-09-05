45c85eef0ad49ff00bb572f2a69d37c74b9bb118
andrew070583 <31409655+andrew070583@users.noreply.github.com>
2017-09-05 17:11:07 +0300

    Add files via upload

---
+++ 3n_numbers.py
@@ -0,0 +1,26 @@
+def func(n):
+    i = 1
+
+    for i in range(1,(n+1)*2):
+       if i % 2 == 0:
+                print(i)
+
+
+
+def func2(n):
+    s = str(n)
+    print()
+    print(len(s))
+
+
+
+
+func(5)
+
+func2(335)
+
+
+
+
+
+
